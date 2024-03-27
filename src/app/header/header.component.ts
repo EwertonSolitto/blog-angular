c73a3e5d20329729728ee47f4078dfc103db60e9
Ewerton <ewerton2004@yahoo.com.br>
2024-03-26 23:41:28 -0300

    feat(header): create a button that open the menu and toggle icon

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatButtonModule, MatMenuModule, MatIconModule],
+  imports: [MatButtonModule, MatMenuModule, MatIconModule, NgIf],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  menuOpened = true
 }
